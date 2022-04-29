59446218587c6305d679dba9446d1895af03f29f
ELVISouza <elvissouza516@gmail.com>
2022-04-29 11:00:26 -0300

    first commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import { CursosModule } from './cursos/cursos.module';
+
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    
+  
+  ],
+  imports: [
+    BrowserModule,
+    CursosModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
